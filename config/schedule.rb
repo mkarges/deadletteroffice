0f022b09e39e11c50bd0504236f7b396bcb78ad5
M Karges <karges@gmail.com>
2011-01-19 23:36:03 -0500

    deleted log file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,11 +1,8 @@
-set :output, "/home/ub/rails/deadletteroffice/config/cron.log"
+#set :output, "/home/ub/rails/deadletteroffice/config/cron.log"
 
 #every 3.hours do
 #   rake "cron", :environment => :development
 #end
 
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
 
-# Learn more: http://github.com/javan/whenever
+#disabled this because app is in production on heroku no use for this file
